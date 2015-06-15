2e5f820fbca2356d2658a5bf997a57a24b408230
My Ho <mydiemho@gmail.com>
2015-06-14 21:23:57 -0700

    testing storm processing

--- storm/request_topology/requestBolt.py
+++ storm/request_topology/requestBolt.py
@@ -1,10 +1,17 @@
+import json
 import logging
-
-__author__ = 'myho'
+import random
+from kafka import KafkaClient, SimpleProducer
+import pyelasticsearch
 
 from pyleus.storm import SimpleBolt
 
+INDEX_NAME = 'taxi_index'
+
 log = logging.getLogger("logging_example.request_bolt")
+es = pyelasticsearch.ElasticSearch()
+client = KafkaClient("localhost:9092")
+producer = SimpleProducer(client)
 
 class RequestBolt(SimpleBolt):
 
@@ -13,6 +20,51 @@ class RequestBolt(SimpleBolt):
     def process_tuple(self, tup):
         request = tup.values
         log.info(request)
+
+        parsed_msg = json.loads(request.value)
+
+        location = {
+            "lat": parsed_msg['location']['latitude'],
+            "lon": parsed_msg['location']['longitude']
+        }
+
+        query = {
+            "from": 0,
+            "size": 1,
+            "query": {
+                "match": {
+                    "is_occupied": "0"
+                }
+            },
+            "sort": [
+                {
+                    "_geo_distance": {
+                        "location": location,
+                        "order": "asc",
+                        "unit": "km"
+                    }
+                }
+            ]
+        }
+
+        # find closest one, send response back to user, let user pick or not
+
+        print "executing search query"
+        res = es.search(query, index=INDEX_NAME)
+        hits = res['hits']['hits']
+        index = random.randint(0, 14)
+        # print "index ", index
+        taxi_id = hits[index]['_id']
+        print "sending occupancy_update event for taxi %s\n" % taxi_id
+        # print json.dumps(hits[index])
+
+        # send to kafka
+        msg = {}
+        msg['taxi_id'] = taxi_id
+
+        producer.send_messages(
+            "occupancy_update",
+            json.dumps(msg))
 
 if __name__ == '__main__':
     RequestBolt().run()
