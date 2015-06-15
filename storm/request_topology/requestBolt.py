34ebd5798d03718c2813e5e2c166f8a6bc3caaf6
My Ho <mydiemho@gmail.com>
2015-06-14 20:49:24 -0700

    debug

--- storm/request_topology/requestBolt.py
+++ storm/request_topology/requestBolt.py
@@ -1,1 +1,14 @@
 __author__ = 'myho'
+
+from pyleus.storm import SimpleBolt
+
+class RequestBolt(SimpleBolt):
+
+    OUTPUT_FIELDS = ['request']
+
+    def process_tuple(self, tup):
+        request = tup.values
+        print request
+
+if __name__ == '__main__':
+    RequestBolt.run()
