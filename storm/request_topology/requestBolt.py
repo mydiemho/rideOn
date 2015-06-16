14b32d1f025ba74077a25a8af3c89ec3b4460183
My Ho <mydiemho@gmail.com>
2015-06-16 12:21:01 -0700

    more debugging

--- storm/request_topology/requestBolt.py
+++ storm/request_topology/requestBolt.py
@@ -1,22 +1,29 @@
 import json
 import logging
 import random
+
 from kafka import KafkaClient, SimpleProducer
 import pyelasticsearch
-
 from pyleus.storm import SimpleBolt
 
 INDEX_NAME = 'taxi_index'
 QUERY_SIZE = 10
 
+# GOTCHA:
+# have to include "http://" and ends with "/", else will throw error
+ELASTIC_SEARCH_CLUSTER = [
+    "http://52.8.145.247:9200/", "http://52.8.148.251:9200/", "http://52.8.158.130:9200/", "http://52.8.162.105:9200/",
+    "http://52.8.153.92:9200/"]
+
+KAFKA_CLUSTER = "52.8.145.247:9092,52.8.148.251:9092,52.8.158.130:9092,52.8.162.105:9092,52.8.153.92:9092"
+
 log = logging.getLogger("request_topology.request_bolt")
 
-es = pyelasticsearch.ElasticSearch()
-client = KafkaClient("52.8.145.247:9092,52.8.148.251:9092,52.8.158.130:9092,52.8.162.105:9092,52.8.153.92:9092")
+es = pyelasticsearch.ElasticSearch(urls=ELASTIC_SEARCH_CLUSTER)
+client = KafkaClient(KAFKA_CLUSTER)
 producer = SimpleProducer(client)
 
 class RequestBolt(SimpleBolt):
-
     OUTPUT_FIELDS = ['request']
 
     def process_tuple(self, tup):
@@ -70,10 +77,10 @@ class RequestBolt(SimpleBolt):
             "occupancy_update",
             json.dumps(msg))
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     logging.basicConfig(
-        level = logging.DEBUG,
+        level=logging.DEBUG,
         filename='/tmp/pyleus/request_bolt.log',
         format="%(message)s",
         filemode='a'
