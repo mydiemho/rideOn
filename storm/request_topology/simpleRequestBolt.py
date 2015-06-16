359c2e25ac24e756b9698368b2d27ba973c37620
My Ho <mydiemho@gmail.com>
2015-06-16 10:56:15 -0700

    added a simplified version of bolt for testing

---
+++ storm/request_topology/simpleRequestBolt.py
@@ -0,0 +1,26 @@
+import json
+import logging
+import random
+from kafka import KafkaClient, SimpleProducer
+import pyelasticsearch
+
+from pyleus.storm import SimpleBolt
+
+INDEX_NAME = 'taxi_index'
+
+log = logging.getLogger("request_topology.request_bolt")
+es = pyelasticsearch.ElasticSearch()
+client = KafkaClient("52.8.145.247:9092,52.8.148.251:9092,52.8.158.130:9092,52.8.162.105:9092,52.8.153.92:9092")
+producer = SimpleProducer(client)
+
+class SimpleRequestBolt(SimpleBolt):
+
+    OUTPUT_FIELDS = ['request']
+
+    def process_tuple(self, tup):
+        request = tup.values
+        log.info("++++++++++++++++++RECEIVING MSG+++++++++++++++")
+        log.info(request)
+
+if __name__ == '__main__':
+    SimpleRequestBolt().run()
