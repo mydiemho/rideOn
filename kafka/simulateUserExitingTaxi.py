cc4297f5b733e87e8842d446336c2b9057102689
My Ho <mydiemho@gmail.com>
2015-06-16 21:31:36 -0700

    add simulation for vacating taxi

--- kafka/simulateUserExitingTaxi.py
+++ kafka/simulateUserExitingTaxi.py
@@ -2,17 +2,18 @@
 
 # Kafka producer that sends events in a loop in order to simulate user vacating taxi
 import json
-import random
-import time
 import sys
 
+from pyelasticsearch import ElasticSearch
+
 from kafka import KafkaClient, SimpleProducer
+import time
+
 
 class UserVacatingTaxiProducer():
     def __init__(self, topic, config_file):
         self.topic = topic
         self.config_file = config_file
-
 
     def genData(self, ):
         with open(self.config_file, 'rb') as config_file:
@@ -21,21 +22,61 @@ class UserVacatingTaxiProducer():
         kafka_cluster = config['kafka_cluster']
         client = KafkaClient(kafka_cluster)
         producer = SimpleProducer(client)
+        es_client = ElasticSearch(config['es_cluster'])
 
         while True:
-            msg = {}
-            msg['taxi_id'] = taxi_id
-            msg['occupancy_status'] = 0
+            # construct query to randomly select an occupied taxi
+            query = {
+                "from": 0,
+                "size": 1,
+                "query": {
+                    "function_score": {
+                        "filter": {
+                            "term": {"is_occupied": "1"}
+                        },
+                        "functions": [
+                            {
+                                "random_score": {}
+                            }
+                        ]
+                    }
+                }
+            }
 
-            producer.send_messages(
-                "occupancy_update",
-                json.dumps(msg))
+            print "++++++++++++executing search query++++++++++++++"
+            res = es_client.search(query, index=config['index'])
 
+            hits = res['hits']['hits']
 
-            producer.send_messages(self.topic, json.dumps(msg))
-            print "sending occupancy_update event for taxi %s\n" % taxi_id
-            time.sleep(30)  # Creating some delay
+            # no occupied taxi
+            if len(hits) == 0:
+                return
 
+            taxi_id = hits["_id"]
+            print ("++++++++++++++++++vacating taxi %s++++++++", taxi_id)
+            taxi_doc = {
+                "is_occupied": "0"
+            }
+
+            taxi_type = 'taxi'
+            res = es_client.update(index=config['index'],
+                            id=taxi_id,
+                            doc=taxi_doc,
+                            doc_type=taxi_type)
+            print "+++++++++++++updated occupancy++++++++++"
+            print res
+
+            # msg = {}
+            # msg['taxi_id'] = taxi_id
+            # msg['occupancy_status'] = 0
+            #
+            # producer.send_messages(
+            #     self.topic,
+            #     json.dumps(msg))
+            #
+            # print "sending occupancy_update event for taxi %s\n" % taxi_id
+
+            time.sleep(5)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -50,7 +91,7 @@ if __name__ == "__main__":
     # logger.addHandler(fh)
     producer = UserVacatingTaxiProducer(
         config_file=sys.argv[1],
-        topic='user_request'
+        topic='occupancy_update'
     )
 
     producer.genData()
