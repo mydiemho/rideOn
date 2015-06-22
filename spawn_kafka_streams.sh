485aaf982b2d3da39dad35c148612923d1d8a6df
My Ho <mydiemho@gmail.com>
2015-06-21 17:48:54 -0700

    add tmux script

---
+++ spawn_kafka_streams.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+NUM_SPAWNS=$1
+SESSION=$2
+tmux new-session -s $SESSION -n bash -d
+for ID in `seq 1 $NUM_SPAWNS`;
+do
+    echo "data/locations" + $(($ID%4)) + ".csv"
+#    tmux new-window -t $ID
+#    tmux send-keys -t $SESSION:$ID 'kafka/taxiGeoProducer.py kafka/config.json data/locations' + $(($ID%4)) + '.csv' C-m
+done
