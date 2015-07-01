756cd9124b7c21a00d8a0cbbd5d559701ca8b35c
My Ho <mydiemho@gmail.com>
2015-06-30 20:20:08 -0700

    update scripts

--- simulations/run.sh
+++ simulations/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 screen ../elasticSearch/toggleOccupiedTaxi.py
-screen ../kafka/bulkLocationProducer.py ../data/locations.csv
+screen ../kafka/bulkLocationProducer.py ../data/1locations.csv
 screen ../kafka/bulkRequestProducer.py ../data/userLocations.csv
---
+++ simulations/stop.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
