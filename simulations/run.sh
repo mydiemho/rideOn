47e0455afd6436d380da651b6a10767c45a2e972
My Ho <mydiemho@gmail.com>
2015-06-30 20:23:40 -0700

    update script

--- simulations/run.sh
+++ simulations/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-screen ../elasticSearch/toggleOccupiedTaxi.py
-screen ../kafka/bulkLocationProducer.py ../data/1locations.csv
-screen ../kafka/bulkRequestProducer.py ../data/userLocations.csv
+screen -d -m -S exitTaxi ../elasticSearch/toggleOccupiedTaxi.py
+screen -d -m -S geo ../kafka/bulkLocationProducer.py ../data/1locations.csv
+screen -d -m -S request ../kafka/bulkRequestProducer.py ../data/userLocations.csv
